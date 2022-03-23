4e1d33235ddfbdfcc171c0d44f315b7289d2f122
luciarizzo <luciarizzodev@gmail.com>
2022-03-23 01:09:31 -0300

    clase 1 ejercicio 4 terminado

---
+++ 1-project-4/1-project-4/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _1_project_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int divisor;
+            int numero;
+            int acumuladorDivisor = 0;
+            int contadorNumPerfectos = 0;
+            for (numero = 1; contadorNumPerfectos < 4; numero++)
+            {
+                acumuladorDivisor = 0;
+                for(divisor = 1; divisor <= (numero / 2); divisor++)
+                {
+                    if((numero % divisor) == 0)
+                    {
+                        acumuladorDivisor += divisor;
+                    }
+                }
+                if (acumuladorDivisor == numero)
+                {
+                    Console.WriteLine("El numero " + numero + " es perfecto");
+                    contadorNumPerfectos++;
+                }
+            }
+        }
+    }
+}
