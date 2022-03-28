2bfe72f38c7bbd76cc942ff6b01438cb20336e11
luciarizzo <luciarizzodev@gmail.com>
2022-03-28 02:58:45 -0300

    ejercicio 3 clase 2 convertir decimal a binario terminado

---
+++ 2-project-3/2-project-3/Conversor.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2_project_3
+{
+    internal class Conversor
+    {
+        public static string ConvertirDecimalABinario(int numeroEntero)
+        {
+            string numeroFormandose = "";
+            int decimalActualizado = numeroEntero;
+            int flagPrimerIngreso = 0;
+            if(numeroEntero > 0)
+            {
+                do
+                {
+                    if (flagPrimerIngreso == 1)
+                    {
+                        decimalActualizado = decimalActualizado / 2;
+                    }
+
+                    flagPrimerIngreso = 1;
+
+                    if ((decimalActualizado % 2) == 0)
+                    {
+
+                        numeroFormandose = "0" + numeroFormandose;
+                    }
+                    else if ((decimalActualizado % 2) == 1)
+                    {
+                        numeroFormandose = "1" + numeroFormandose;
+                    }  
+                } while (decimalActualizado > 1);
+            }
+            else
+            {
+                Console.WriteLine("Error. Por favor, ingrese un numero positivo");
+            }
+            Console.WriteLine("El numero {0} en Binario es: {1}", numeroEntero, numeroFormandose);
+            return numeroFormandose;
+        }
+
+        public static int ConvertirBinarioADecimal(int numeroEntero)
+        {
+            string numero = numeroEntero.ToString();
+            int tamanio = numero.Length;
+            for(int i = 0; i <= tamanio; i++)
+            {
+                i = Math.Pow(2, i);
+
+            }
+
+        }
+    }
+}
---
+++ 2-project-3/2-project-3/Program.cs
@@ -0,0 +1,37 @@
+﻿/*
+ * Desarollar una clase llamada Conversor que posea dos métodos de clase (estáticos):
+
+El método ConvertirDecimalABinario que convierte un número entero del sistema decimal al sistema binario.
+
+El método ConvertirBinarioADecimal que convierte un número entero del sistema binario al sistema decimal.
+
+ */
+
+using System;
+
+namespace _2_project_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numeroEntero;
+            char respuesta;
+            Console.WriteLine("Ingrese A para convertir de Decimal a Binario o B para convertir de Binario a Decimal: ");
+            Char.TryParse(Console.ReadLine(), out respuesta);
+            switch (respuesta)
+            {
+                case 'A':
+                    Console.WriteLine("Ingrese un numero para convertir de Decimal a BSinario: ");
+                    int.TryParse(Console.ReadLine(), out numeroEntero);
+                    Conversor.ConvertirDecimalABinario(numeroEntero);
+                    break;
+                case 'B':
+                    Console.WriteLine("Ingrese un numero para convertir de Binario a Decimal: ");
+                    int.TryParse(Console.ReadLine(), out numeroEntero);
+                    Conversor.ConvertirBinarioADecimal(numeroEntero);
+                    break;
+            }
+        }
+    }
+}
