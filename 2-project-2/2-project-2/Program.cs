ea379dd473db12ef00eaf09968e7c5b93cdfad1a
luciarizzo <luciarizzodev@gmail.com>
2022-03-28 01:03:50 -0300

    ejercicio 2 clase 2 hecho

---
+++ 2-project-2/2-project-2/Program.cs
@@ -0,0 +1,33 @@
+﻿/*
+ * Realizar un programa que sume números enteros hasta que el usuario lo determine por medio de un mensaje 
+ * "¿Desea continuar? (S/N)".
+
+Crear la clase Validador y el método estático ValidarRespuesta, que validará el ingreso de respuestas.
+
+El método devolverá un valor de tipo booleano, TRUE si se ingresó una 'S' y FALSE si se ingresó cualquier otro valor.
+ */
+
+using System;
+
+namespace _2_project_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char respuesta;
+            int suma = 0;
+            int numero;
+            do
+            {
+                Console.WriteLine("Ingrese un numero para sumar: ");
+                int.TryParse(Console.ReadLine(), out numero);
+                suma += numero;
+                Console.WriteLine("La suma hasta ahora es: {0} ", suma);
+                Console.WriteLine("¿Desea continuar? Ingrese S o N");
+                Char.TryParse(Console.ReadLine(), out respuesta);
+
+            } while (Validador.ValidarRespuesta(respuesta));
+        }
+    }
+}
---
+++ 2-project-2/2-project-2/Validador.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2_project_2
+{
+    internal class Validador
+    {
+        public static bool ValidarRespuesta(char respuesta)
+        {
+            bool validado = false;
+            if(respuesta == 'S')
+            {
+                validado = true;
+            }
+            return validado;
+        }
+    }
+}
