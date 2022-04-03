93ac9c96e0c30dda6f7093e3510425b3e514e20c
luciarizzo <luciarizzodev@gmail.com>
2022-04-03 11:31:28 -0300

    queda arreglar edad

---
+++ 3-project-2/3-project-2/Program.cs
@@ -0,0 +1,52 @@
+﻿/*
+Crear una aplicación de consola y una biblioteca de clases que contenga la clase Persona.
+
+Deberá tener los atributos:
+
+nombre
+fechaDeNacimiento
+dni
+
+Deberá tener un constructor que inicialice todos los atributos.
+
+Construir los siguientes métodos para la clase:
+
+Setter y getter para cada uno de los atributos.
+
+CalcularEdad será privado y retornará la edad de la persona calculándola a partir de la 
+fecha de nacimiento.
+
+Mostrar retornará una cadena de texto con todos los datos de la persona, incluyendo 
+la edad actual.
+
+EsMayorDeEdad si es mayor de edad devuelve el valor “Es mayor de edad", sino devuelve “es menor”.
+
+Instanciar 3 objetos de tipo Persona en el método Main.
+
+Mostrar quiénes son mayores de edad y quiénes no.
+ */
+
+using System;
+using Logica;
+
+namespace _3_project_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Persona gaston = new Persona("Gaston", new DateTime(1988, 03, 09), "21563486");
+            Persona noelia = new Persona("Noelia", new DateTime(1999, 05, 23), "41568752");
+            Persona pedro = new Persona("Pedro", new DateTime(1997, 09, 05), "29745314");
+
+            Console.WriteLine(gaston.Mostrar());
+            Console.WriteLine(noelia.Mostrar());
+            Console.WriteLine(pedro.Mostrar());
+
+            gaston.MostrarEsMayorOMenor();
+            noelia.MostrarEsMayorOMenor();
+            pedro.MostrarEsMayorOMenor();
+
+        }
+    }
+}
---
+++ 3-project-2/Logica/Persona.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Text;
+
+namespace Logica
+{
+    public class Persona
+    {
+        private string nombre;
+        private DateTime fechaDeNacimiento;
+        private string dni;
+        private int edad;
+
+        public Persona(string nombre, DateTime fechaDeNacimiento, string dni)
+        {
+            SetNombre(nombre);
+            this.fechaDeNacimiento = fechaDeNacimiento;
+            this.dni = dni;
+        }
+
+        public void SetNombre(string nombre)
+        {
+            if (string.IsNullOrWhiteSpace(nombre))
+            {
+                nombre = "Sin nombre";
+            }
+            this.nombre = nombre.Trim();
+        }
+
+        public void SetFechaDeNacimiento(int dia, int mes, int anio)
+        {
+            fechaDeNacimiento = new DateTime(dia, mes, anio);
+        }
+
+        public void SetDni(string dni)
+        {
+            this.dni = dni;
+        }
+
+        public int CalcularEdad()
+        {
+            //DateTime fechaActual = DateTime.Now;
+            //int anioActual = fechaActual.Year;
+            //int anioNacimiento = fechaDeNacimiento.Year;
+            int edad = DateTime.Now.Year - fechaDeNacimiento.Year;
+            return edad;
+        }
+
+        public void SetEdad()
+        {
+            this.edad = CalcularEdad();
+        }
+
+        public string GetFechaDeNacimiento()
+        {
+            StringBuilder sb = new StringBuilder();
+            return sb.ToString();
+        }
+
+        public string GetNombre()
+        {
+            return nombre;
+        }
+
+        public string GetDni()
+        {
+            return dni;
+        }
+
+
+
+        public string Mostrar()
+        {
+
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"Nombre de la persona: {nombre}");
+            sb.AppendLine($"La edad de la persona es: {edad}");
+            sb.AppendLine($"DNI de la persona: {dni}");
+            sb.AppendLine($"Fecha de nacimiento: {fechaDeNacimiento.ToString("dd/MM/yyyy")}");
+
+
+            return sb.ToString();
+
+        }
+
+        public bool EsMayorDeEdad()
+        {
+            if (edad > 17)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+            
+        }
+
+        public void MostrarEsMayorOMenor()
+        {
+            if (EsMayorDeEdad() == true)
+            {
+                Console.WriteLine("Es mayor de edad");
+            }
+            else if(EsMayorDeEdad() == false)
+            {
+                Console.WriteLine("Es menor de edad");
+            }
+        }
+    }
+}
