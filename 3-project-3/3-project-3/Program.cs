16406bd09d3d879431e87237196595498e5c85c7
luciarizzo <luciarizzodev@gmail.com>
2022-04-03 21:50:19 -0300

    ejercicio 3 clase 3 realizado

---
+++ 3-project-3/3-project-3/Program.cs
@@ -0,0 +1,37 @@
+﻿/*
+ * 
+ * 
+ */
+
+using System;
+using Logica;
+
+namespace _3_project_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Estudiante estudiante1 = new Estudiante("Argibay", "Gaston", "45785");
+            Estudiante estudiante2 = new Estudiante("Zeitunlian", "Noelia", "56984");
+            Estudiante estudiante3 = new Estudiante("Fleming", "Pedro", "65785");
+
+            estudiante1.SetNotaPrimerParcial(8); 
+            estudiante2.SetNotaPrimerParcial(7);
+            estudiante3.SetNotaPrimerParcial(10);
+
+            estudiante1.SetNotaSegundoParcial(1);
+            estudiante2.SetNotaSegundoParcial(6);
+            estudiante3.SetNotaSegundoParcial(9);
+
+            estudiante1.CalcularNotaFinal();
+            estudiante2.CalcularNotaFinal();
+            estudiante3.CalcularNotaFinal();
+
+            Console.WriteLine(estudiante1.Mostrar());
+            Console.WriteLine(estudiante2.Mostrar());
+            Console.WriteLine(estudiante3.Mostrar());
+
+        }
+    }
+}
---
+++ 3-project-3/Logica/Estudiante.cs
@@ -0,0 +1,77 @@
+﻿/*
+ * 
+ */
+
+using System;
+using System.Text;
+
+namespace Logica
+{
+    public class Estudiante
+    {
+        private string apellido;
+        private string legajo;
+        private string nombre;
+        private int notaPrimerParcial;
+        private int notaSegundoParcial;
+        private float promedio;
+        private double notaFinal;
+        private Random random;
+
+        public Estudiante(string apellido, string nombre, string legajo)
+        {
+            this.apellido = apellido;
+            this.legajo = legajo;
+            this.nombre = nombre;
+        }
+
+        public void SetNotaPrimerParcial(int notaPrimerParcial)
+        {
+            this.notaPrimerParcial = notaPrimerParcial;
+        }
+
+        public void SetNotaSegundoParcial(int notaSegundoParcial)
+        {
+            this.notaSegundoParcial = notaSegundoParcial;
+        }
+
+        private float CalcularPromedio()
+        {
+            promedio = (notaPrimerParcial + notaSegundoParcial) / 2;
+            return promedio;
+        }
+
+        public void CalcularNotaFinal()
+        {
+            var random = new Random();
+            double notaFinal;
+            if(notaPrimerParcial >= 4 && notaSegundoParcial >= 4)
+            {
+                notaFinal = random.Next(6, 10);
+            }
+            else
+            {
+                notaFinal = -1;
+            }
+            this.notaFinal = notaFinal;
+        }
+
+        public string Mostrar()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"Nombre: {nombre}. Apellido: {apellido}. Legajo: {legajo}");
+            sb.AppendLine($"Nota primer parcial: {notaPrimerParcial}. Nota segundo parcial: {notaSegundoParcial}");
+            sb.AppendLine($"Promedio: {CalcularPromedio()}");
+            if(notaFinal != -1)
+            {
+                sb.AppendLine($"Nota final: {notaFinal}");
+            }
+            else
+            {
+                sb.AppendLine($"Alumno desaprobado");
+            }
+            return sb.ToString();
+
+        }
+    }
+}
